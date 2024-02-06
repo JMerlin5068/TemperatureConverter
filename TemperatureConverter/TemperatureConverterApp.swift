99afd804e5107bab503732edb9be5c674a407c8e
Jarrod Merlin <jarrod.merlin@stu.barren.kyschools.us>
2024-02-06 08:50:14 -0600

    Initial Commit

---
+++ TemperatureConverter/TemperatureConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConverterApp.swift
+//  TemperatureConverter
+//
+//  Created by Student on 2/6/24.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
